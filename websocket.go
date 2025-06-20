d9125c09cff4d46bdf02ae66533c340eed136edc
Simon Striekwold <simon.striekwold@joule-technologies.com>
2025-06-20 12:09:48 +0200

    Allow passing of DialContext to websocket dialer

--- websocket.go
+++ websocket.go
@@ -1,6 +1,7 @@
 package mqtt
 
 import (
+	"context"
 	"crypto/tls"
 	"fmt"
 	"io"
@@ -18,6 +19,7 @@ type WebsocketOptions struct {
 	ReadBufferSize  int
 	WriteBufferSize int
 	Proxy           ProxyFunction
+	NetDialContext func(ctx context.Context, network, addr string) (net.Conn, error)
 }
 
 type ProxyFunction func(req *http.Request) (*url.URL, error)
@@ -43,6 +45,7 @@ func NewWebsocket(host string, tlsc *tls.Config, timeout time.Duration, requestH
 		Subprotocols:      []string{"mqtt"},
 		ReadBufferSize:    options.ReadBufferSize,
 		WriteBufferSize:   options.WriteBufferSize,
+		NetDialContext:    options.NetDialContext,
 	}
 
 	ws, resp, err := dialer.Dial(host, requestHeader)
